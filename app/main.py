58a1e40dd64dc8009495379c44f2720c65c9ee09
rongying <kohrongying@gmail.com>
2022-08-08 01:03:46 +0800

    [2]Send PONG in RESP

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
+    server_socket.send("+PONG\r\n")
 
 
 if __name__ == "__main__":
