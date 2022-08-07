5c67bb96a77e82a3b4d80a875271a9e123b79866
rongying <kohrongying@gmail.com>
2022-08-08 01:09:28 +0800

    [2] Pass test

--- app/main.py
+++ app/main.py
@@ -6,8 +6,10 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-    server_socket.send("+PONG\r\n")
+    client_connection, _ = server_socket.accept() # wait for client
+
+    client_connection.recv(1024) # data received from client
+    client_connection.send(b"+PONG\r\n") # hardcode pong with RESP
 
 
 if __name__ == "__main__":
