f49bf99eeb0e288f0c03e608874441501e8257ed
rongying <kohrongying@gmail.com>
2022-08-08 14:37:28 +0800

    [4] Add threading

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 import socket
+import threading
+
+
+def ping_function(conn):
+    conn.recv(1024)  # data received from client
+    conn.send(b"+PONG\r\n")  # hardcode pong with RESP
 
 
 def main():
@@ -6,11 +12,11 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_connection, _ = server_socket.accept() # wait for client
+    client_connection, _ = server_socket.accept()  # wait for client
 
     while True:
-        client_connection.recv(1024) # data received from client
-        client_connection.send(b"+PONG\r\n") # hardcode pong with RESP
+        x = threading.Thread(target=ping_function, args=(client_connection,))
+        x.start()
 
 
 if __name__ == "__main__":
