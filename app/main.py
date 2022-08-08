441b88fb6d81e01842510f004680cc3a577d3b59
rongying <kohrongying@gmail.com>
2022-08-08 14:31:14 +0800

    [3] Add while loop

--- app/main.py
+++ app/main.py
@@ -8,8 +8,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_connection, _ = server_socket.accept() # wait for client
 
-    client_connection.recv(1024) # data received from client
-    client_connection.send(b"+PONG\r\n") # hardcode pong with RESP
+    while True:
+        client_connection.recv(1024) # data received from client
+        client_connection.send(b"+PONG\r\n") # hardcode pong with RESP
 
 
 if __name__ == "__main__":
